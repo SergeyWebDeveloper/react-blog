6d2591ea8ad7f894b00088881e7c232ea141aa5d
Sergey Kozlov <sergeykozlov.2011@yandex.ru>
2018-03-14 00:41:12 +0300

    add redirect to page

--- src/pages/PageSignUp.js
+++ src/pages/PageSignUp.js
@@ -1,24 +1,37 @@
 import React, {Component} from 'react';
-import axios from 'axios';
+import {Redirect} from 'react-router-dom';
 import SignUp from '../components/SignUpForm';
 import {createUser} from '../api';
 
 class PageSignUp extends Component {
 	state={
-		error: false
+		error: false,
+		message: '',
+		success: false
 	};
 
 	submitForm = async value =>{
+		this.setState({
+			error: false,
+			message: '',
+			success: false
+		});
 		const {data} = await createUser(value);
-		console.log(data);
-		// if(data.error){
-		// 	this.setState({error: true});
-		// }
+		if(data.error){
+			this.setState({error: true});
+		} else if(data.user){
+			this.setState({
+				error: true,
+				message: 'Такой пользователь уже существует!'
+			});
+		} else {
+			this.setState({success: true});
+		}
 	};
 
 	messageErrorShow = () =>{
 		return(
-			<p>В резальтате сохранения произошла ошибка! Попробуйте заново.</p>
+			<p>В резальтате сохранения произошла ошибка! Попробуйте заново. {this.state.message}</p>
 		);
 	};
 
@@ -27,7 +40,8 @@ class PageSignUp extends Component {
 			<div className='container'>
 				<h1>Зарегистрироваться</h1>
 				<SignUp onSubmit={this.submitForm} />
-				{this.state.error?this.messageErrorShow():null}
+				{this.state.error&&this.messageErrorShow()}
+				{this.state.success&&<Redirect to='/signin'/>}
 			</div>
 		)
 	}
